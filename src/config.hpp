891fe802024011eb579d7a80b0f9bdbc0bfc8c5a
destroyer <destroyer.dcf@gmail.com>
2024-12-22 11:00:33 +0100

    add swich

--- src/config.hpp
+++ src/config.hpp
@@ -9,17 +9,4 @@
 #define STOP_EJECT_SWICH A2
 #define PAUSE_SWICH A3
 
-// Scancodes Gamepad
-const int ESP_JOY1LEFT = 0x40;
-const int ESP_JOY1RIGHT = 0x41;
-const int ESP_JOY1UP = 0x42;
-const int ESP_JOY1DOWN = 0x43;
-const int ESP_JOY1START = 0x44;
-const int ESP_JOY1MODE = 0x45;
-const int ESP_JOY1A = 0x46;
-const int ESP_JOY1B = 0x47;
-const int ESP_JOY1C = 0x48;
-const int ESP_JOY1X = 0x49;
-const int ESP_JOY1Y = 0x4a;
-const int ESP_JOY1Z = 0x4b;
 
--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ bool ffPressed = false;
 bool stopEjectPressed = false;
 bool pausePressed = false;
 
-void sendKeyAction(int scancode, bool press) {
+void sendGamePadAction(int scancode, bool press) {
   keyboard.write(0xE2);
   delay(15);
   if (!press) {
@@ -48,179 +48,25 @@ void sendKeyAction(int scancode, bool press) {
   delay(15);
 }
 
+void sendKeyBoardAction(int scancode, bool press) {
+  if (press) {
+    keyboard.write(scancode);
+  } else {
+    keyboard.write(0xF0); // Código de liberación
+    delay(15);
+    keyboard.write(scancode);
+  }
+  delay(15);
+}
+
 void cassetteState() {
-  printf("Cassette state\n");
-}
-
-void gamePadState()
-{
-    // Verificar el cambio de estado para cada botón
-    if ((currentState & SC_BTN_UP) != (lastState & SC_BTN_UP)) {
-        if (currentState & SC_BTN_UP) {
-            Serial.println("Botón UP presionado");
-            sendKeyAction(ESP_JOY1UP, true);
-        } else {
-            Serial.println("Botón UP soltado");
-            sendKeyAction(ESP_JOY1UP, false);
-        }
-    }
-
-    if ((currentState & SC_BTN_DOWN) != (lastState & SC_BTN_DOWN)) {
-        if (currentState & SC_BTN_DOWN) {
-            sendKeyAction(ESP_JOY1DOWN, true);
-            Serial.println("Botón DOWN presionado");
-        } else {
-            sendKeyAction(ESP_JOY1DOWN, false);
-            Serial.println("Botón DOWN soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_LEFT) != (lastState & SC_BTN_LEFT)) {
-        if (currentState & SC_BTN_LEFT) {
-            sendKeyAction(ESP_JOY1LEFT, true);
-            Serial.println("Botón LEFT presionado");
-        } else {
-          sendKeyAction(ESP_JOY1LEFT, false);
-            Serial.println("Botón LEFT soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_RIGHT) != (lastState & SC_BTN_RIGHT)) {
-        if (currentState & SC_BTN_RIGHT) {
-            sendKeyAction(ESP_JOY1RIGHT, true);
-            Serial.println("Botón RIGHT presionado");
-        } else {
-            sendKeyAction(ESP_JOY1RIGHT, false);
-            Serial.println("Botón RIGHT soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_START) != (lastState & SC_BTN_START)) {
-        if (currentState & SC_BTN_START) {
-            sendKeyAction(ESP_JOY1START, true);
-            Serial.println("Botón START presionado");
-        } else {
-            sendKeyAction(ESP_JOY1START, false);
-            Serial.println("Botón START soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_A) != (lastState & SC_BTN_A)) {
-        if (currentState & SC_BTN_A) {
-            sendKeyAction(ESP_JOY1A, true);
-            Serial.println("Botón A presionado");
-        } else {
-            sendKeyAction(ESP_JOY1A, false);
-            Serial.println("Botón A soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_B) != (lastState & SC_BTN_B)) {
-        if (currentState & SC_BTN_B) {
-            sendKeyAction(ESP_JOY1B, true);
-            Serial.println("Botón B presionado");
-        } else {
-            sendKeyAction(ESP_JOY1B, false);
-            Serial.println("Botón B soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_C) != (lastState & SC_BTN_C)) {
-        if (currentState & SC_BTN_C) {
-            sendKeyAction(ESP_JOY1C, true);
-            Serial.println("Botón C presionado");
-        } else {
-            sendKeyAction(ESP_JOY1C, false);
-            Serial.println("Botón C soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_X) != (lastState & SC_BTN_X)) {
-        if (currentState & SC_BTN_X) {
-            sendKeyAction(ESP_JOY1X, true);
-            Serial.println("Botón X presionado");
-        } else {
-            sendKeyAction(ESP_JOY1X, false);
-            Serial.println("Botón X soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_Y) != (lastState & SC_BTN_Y)) {
-        if (currentState & SC_BTN_Y) {
-            sendKeyAction(ESP_JOY1Y, true);
-            Serial.println("Botón Y presionado");
-        } else {
-            sendKeyAction(ESP_JOY1Y, false);
-            Serial.println("Botón Y soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_Z) != (lastState & SC_BTN_Z)) {
-        if (currentState & SC_BTN_Z) {
-            sendKeyAction(ESP_JOY1Z, true);
-            Serial.println("Botón Z presionado");
-        } else {
-            sendKeyAction(ESP_JOY1Z, false);
-            Serial.println("Botón Z soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_1) != (lastState & SC_BTN_1)) {
-        if (currentState & SC_BTN_1) {
-            sendKeyAction(ESP_JOY1A, true);
-            Serial.println("Botón 1 presionado");
-        } else {
-            sendKeyAction(ESP_JOY1A, false);
-            Serial.println("Botón 1 soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_2) != (lastState & SC_BTN_2)) {
-        if (currentState & SC_BTN_2) {
-            sendKeyAction(ESP_JOY1Z, true);
-            Serial.println("Botón 2 presionado");
-        } else {
-            sendKeyAction(ESP_JOY1Z, false);
-            Serial.println("Botón 2 soltado");
-        }
-    }
-
-    if ((currentState & SC_BTN_MODE) != (lastState & SC_BTN_MODE)) {
-        if (currentState & SC_BTN_MODE) {
-            sendKeyAction(ESP_JOY1MODE, true);
-            Serial.println("Botón MODE presionado");
-        } else {
-            sendKeyAction(ESP_JOY1MODE, false);
-            Serial.println("Botón MODE soltado");
-        }
-    }
-
-    // Actualizar el último estado
-    lastState = currentState;
-}
-
-
-void setup() {
-
-    Serial.begin(9600);
-
-    pinMode(SELECT_SWICH, INPUT_PULLUP);
-    pinMode(REC_SWICH, INPUT_PULLUP);
-    pinMode(PLAY_SWICH, INPUT_PULLUP);
-    pinMode(REW_SWICH, INPUT_PULLUP);
-    pinMode(FF_SWICH, INPUT_PULLUP);
-    pinMode(STOP_EJECT_SWICH, INPUT_PULLUP);
-    pinMode(PAUSE_SWICH, INPUT_PULLUP);
-}
-
-void loop()
-{
-    currentState = controller.getState();
-    gamePadState();
 
     byte swichState = digitalRead(SELECT_SWICH);
     if (swichState == LOW && !selectPressed) {
         Serial.println("SELECT is pressed");
+        // sendKeyBoardAction(PS2dev::F5, true);
+        sendKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, true);  // Presionar UP_ARROW
+
         selectPressed = true;
     } else if (swichState == HIGH) {
         selectPressed = false;
@@ -273,7 +119,174 @@ void loop()
     } else if (swichState == HIGH) {
         pausePressed = false;
     }
+}
 
+void gamePadState()
+{
+    // Verificar el cambio de estado para cada botón
+    if ((currentState & SC_BTN_UP) != (lastState & SC_BTN_UP)) {
+        if (currentState & SC_BTN_UP) {
+            Serial.println("Botón UP presionado");
+            sendGamePadAction(PS2dev::ESP_JOY1UP, true);
+        } else {
+            Serial.println("Botón UP soltado");
+            sendGamePadAction(PS2dev::ESP_JOY1UP, false);
+        }
+    }
+
+    if ((currentState & SC_BTN_DOWN) != (lastState & SC_BTN_DOWN)) {
+        if (currentState & SC_BTN_DOWN) {
+            sendGamePadAction(PS2dev::ESP_JOY1DOWN, true);
+            Serial.println("Botón DOWN presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1DOWN, false);
+            Serial.println("Botón DOWN soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_LEFT) != (lastState & SC_BTN_LEFT)) {
+        if (currentState & SC_BTN_LEFT) {
+            sendGamePadAction(PS2dev::ESP_JOY1LEFT, true);
+            Serial.println("Botón LEFT presionado");
+        } else {
+          sendGamePadAction(PS2dev::ESP_JOY1LEFT, false);
+            Serial.println("Botón LEFT soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_RIGHT) != (lastState & SC_BTN_RIGHT)) {
+        if (currentState & SC_BTN_RIGHT) {
+            sendGamePadAction(PS2dev::ESP_JOY1RIGHT, true);
+            Serial.println("Botón RIGHT presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1RIGHT, false);
+            Serial.println("Botón RIGHT soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_START) != (lastState & SC_BTN_START)) {
+        if (currentState & SC_BTN_START) {
+            sendGamePadAction(PS2dev::ESP_JOY1START, true);
+            Serial.println("Botón START presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1START, false);
+            Serial.println("Botón START soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_A) != (lastState & SC_BTN_A)) {
+        if (currentState & SC_BTN_A) {
+            sendGamePadAction(PS2dev::ESP_JOY1A, true);
+            Serial.println("Botón A presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1A, false);
+            Serial.println("Botón A soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_B) != (lastState & SC_BTN_B)) {
+        if (currentState & SC_BTN_B) {
+            sendGamePadAction(PS2dev::ESP_JOY1B, true);
+            Serial.println("Botón B presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1B, false);
+            Serial.println("Botón B soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_C) != (lastState & SC_BTN_C)) {
+        if (currentState & SC_BTN_C) {
+            sendGamePadAction(PS2dev::ESP_JOY1C, true);
+            Serial.println("Botón C presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1C, false);
+            Serial.println("Botón C soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_X) != (lastState & SC_BTN_X)) {
+        if (currentState & SC_BTN_X) {
+            sendGamePadAction(PS2dev::ESP_JOY1X, true);
+            Serial.println("Botón X presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1X, false);
+            Serial.println("Botón X soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_Y) != (lastState & SC_BTN_Y)) {
+        if (currentState & SC_BTN_Y) {
+            sendGamePadAction(PS2dev::ESP_JOY1Y, true);
+            Serial.println("Botón Y presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1Y, false);
+            Serial.println("Botón Y soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_Z) != (lastState & SC_BTN_Z)) {
+        if (currentState & SC_BTN_Z) {
+            sendGamePadAction(PS2dev::ESP_JOY1Z, true);
+            Serial.println("Botón Z presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1Z, false);
+            Serial.println("Botón Z soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_1) != (lastState & SC_BTN_1)) {
+        if (currentState & SC_BTN_1) {
+            sendGamePadAction(PS2dev::ESP_JOY1A, true);
+            Serial.println("Botón 1 presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1A, false);
+            Serial.println("Botón 1 soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_2) != (lastState & SC_BTN_2)) {
+        if (currentState & SC_BTN_2) {
+            sendGamePadAction(PS2dev::ESP_JOY1Z, true);
+            Serial.println("Botón 2 presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1Z, false);
+            Serial.println("Botón 2 soltado");
+        }
+    }
+
+    if ((currentState & SC_BTN_MODE) != (lastState & SC_BTN_MODE)) {
+        if (currentState & SC_BTN_MODE) {
+            sendGamePadAction(PS2dev::ESP_JOY1MODE, true);
+            Serial.println("Botón MODE presionado");
+        } else {
+            sendGamePadAction(PS2dev::ESP_JOY1MODE, false);
+            Serial.println("Botón MODE soltado");
+        }
+    }
+
+    // Actualizar el último estado
+    lastState = currentState;
+}
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    pinMode(SELECT_SWICH, INPUT_PULLUP);
+    pinMode(REC_SWICH, INPUT_PULLUP);
+    pinMode(PLAY_SWICH, INPUT_PULLUP);
+    pinMode(REW_SWICH, INPUT_PULLUP);
+    pinMode(FF_SWICH, INPUT_PULLUP);
+    pinMode(STOP_EJECT_SWICH, INPUT_PULLUP);
+    pinMode(PAUSE_SWICH, INPUT_PULLUP);
+}
+
+void loop()
+{
+    currentState = controller.getState();
+    gamePadState();
+    cassetteState();
     delay(100);
 
 }
