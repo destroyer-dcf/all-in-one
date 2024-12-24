e4756cf197545bae3a99ea4ac29b61f190b7c3be
destroyer <destroyer.dcf@gmail.com>
2024-12-24 01:16:01 +0100

    add library keypad

--- src/config.hpp
+++ src/config.hpp
@@ -1,12 +1,12 @@
 #define PS2_CLK_PIN 1
 #define PS2_DATA_PIN 0
 
-#define SELECT_SWICH 16
-#define REC_SWICH 14
-#define PLAY_SWICH 15
-#define REW_SWICH A0
-#define FF_SWICH A1
-#define STOP_EJECT_SWICH A2
-#define PAUSE_SWICH A3
+// #define SELECT_SWICH 16
+// #define REC_SWICH 14
+// #define PLAY_SWICH 15
+// #define REW_SWICH A0
+// #define FF_SWICH A1
+// #define STOP_EJECT_SWICH A2
+// #define PAUSE_SWICH A3
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,23 @@
 #include <ps2dev.h> 
 #include <config.hpp>
 #include <Keypad.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+// #define SCREEN_WIDTH 128 // OLED display width, in pixels
+// #define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// // The pins for I2C are defined by the Wire-library. 
+// // On an arduino UNO:       A4(SDA), A5(SCL)
+// // On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+// #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+// #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 //
 // 5 4 3 2 1
@@ -18,7 +35,7 @@
 // DB9 Pin 8 --> Arduino GND
 // DB9 Pin 9 --> Arduino Pin 8
 
-GamePad controller(7, 2, 3, 4, 5, 6, 8);
+GamePad controller(7, 15, 14, 4, 5, 6, 8);
 
 const byte filas = 2; 
 const byte columnas = 4;
@@ -390,7 +407,12 @@ void gamePadState()
 void setup() {
 
     Serial.begin(9600);
-
+    // display.clearDisplay();
+    // display.setTextSize(2);             // Normal 1:1 pixel scale
+    // display.setTextColor(SSD1306_WHITE);        // Draw white text
+    // display.setCursor(0,0);             // Start at top-left corner
+    // display.println("hola mundo");
+    // display.display();
     // pinMode(SELECT_SWICH, INPUT_PULLUP);
     // pinMode(REC_SWICH, INPUT_PULLUP);
     // pinMode(PLAY_SWICH, INPUT_PULLUP);
