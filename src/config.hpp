9f815ef88bf3c2db1bb1e5da2f384e79c8435dc8
destroyer <destroyer.dcf@gmail.com>
2024-12-28 09:52:35 +0100

    change code

--- src/config.hpp
+++ src/config.hpp
@@ -9,4 +9,17 @@
 // #define STOP_EJECT_SWICH A2
 // #define PAUSE_SWICH A3
 
-
+extern uint8_t SmallFont[];
+extern uint8_t disk32[];
+extern uint8_t mouse32[];
+extern uint8_t snapshot32[];
+extern uint8_t cassette32[];
+extern uint8_t joystick32[];
+extern uint8_t play[];
+extern uint8_t rew[];
+extern uint8_t stop[];
+extern uint8_t ff[];
+extern uint8_t pause[];
+extern uint8_t up[];
+extern uint8_t down[];
+extern uint8_t clearImage[];
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <config.hpp>
 #include <Keypad.h>
 #include <OLED_I2C.h>
-
+#include <avr/pgmspace.h>
 
 // GAMEPAD
 // 5 4 3 2 1
@@ -24,10 +24,6 @@ GamePad controller(7, 15, 14, 4, 5, 6, 8);
 
 // OLED
 OLED  myOLED(SDA, SCL);
-extern uint8_t SmallFont[]; //Tipo de letra
-extern uint8_t TinyFont[]; //Tipo de letra
-
-
 
 // Definir el teclado de la cinta
 const byte filas = 2; 
@@ -37,7 +33,7 @@ byte pinesColumnas[] = {A0,A1,A2,A3};
 char teclas[2][4] = {{'1','2','3','4'},
                      {'5','6','7','8'}};
 
-Keypad cassette = Keypad( makeKeymap(teclas), pinesFilas, pinesColumnas, filas, columnas); 
+Keypad keypad = Keypad( makeKeymap(teclas), pinesFilas, pinesColumnas, filas, columnas); 
 
 // Crear una instancia de PS2dev para el teclado PS2
 PS2dev keyboard(PS2_CLK_PIN, PS2_DATA_PIN);
@@ -106,10 +102,6 @@ void StopEject() {
     // sendKeyBoardAction(PS2dev::F6, true);    
 }
 
-void Rec() {
-    keyCapActive = "REC";
-    sendKeyBoardAction(PS2dev::F4, true);
-}   
 
 void Insert() {
     keyCapActive = "INSERT";
@@ -118,59 +110,6 @@ void Insert() {
     sendKeyBoardAction(PS2dev::F5, true);    
 }
 
-void Play() {
-    keyCapActive = "PLAY";
-    sendKeyBoardAction(PS2dev::F6, true);
-}
-void Pause() {
-    keyCapActive = "PAUSE";
-    keyboard.write(0xE1);
-    delay(15);
-    keyboard.write(0x14);
-    delay(15);
-    keyboard.write(0x77);
-    delay(15);
-    keyboard.write(0xE1);
-    delay(15);
-    keyboard.write(0xF0);
-    delay(15);
-    keyboard.write(0x14);
-    delay(15);
-    keyboard.write(0xF0);
-    delay(15);
-    keyboard.write(0x77);
-    delay(15);
-}
-
-void Rew() {
-    keyCapActive = "REW";
-    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, true);  // Presionar UP_ARROW
-}
-
-void FF() {
-    keyCapActive = "FF";
-    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, true);  // Presionar UP_ARROW
-}
-
-void select() {
-    if (selectValue == "CASSETTE") {
-        selectValue = "SNA";
-
-    } else if (selectValue == "SNA") {
-        selectValue = "CUMSTON SNA";
-    } else if (selectValue == "CUMSTON SNA") {
-        selectValue = "RAPIDA";
-    } else if (selectValue == "RAPIDA") {
-        selectValue = "CASSETTE";
-    }
-
-    Serial.println("--- " + selectValue + " SELECTED");
-
-    if (HardwareDisplay == "OLED") {
-        displayTextOLED(selectValue);
-    }
-
-}
 
 void gamePadState()
 {
@@ -319,49 +258,316 @@ void gamePadState()
     lastState = currentState;
 }
 
+void paintTape() {
+    myOLED.drawBitmap(10, 28, cassette32, 32, 32);
+    myOLED.print("      ", 11, 22);
+    myOLED.print("N", 24, 22);
+    myOLED.drawLine(10,26,22,26);
+    myOLED.drawLine(32,26,42,26);
+    myOLED.update();
+}
 
+void paintTapeRapid() {
+    myOLED.drawBitmap(10, 28, cassette32, 32, 32);
+    myOLED.print("      ", 11, 22);
+    myOLED.print("R", 24, 22);
+    myOLED.drawLine(10,26,22,26);
+    myOLED.drawLine(32,26,42,26);
+    myOLED.update();
+}
+
+void paintSnap() {
+    myOLED.drawBitmap(10, 30, snapshot32, 32, 29);
+    myOLED.print("SNAP", 11, 22);
+    myOLED.update();
+}
+
+void paintSnapCumston() {
+    myOLED.drawBitmap(10, 30, snapshot32, 32, 29);
+    myOLED.print("SNAP", 11, 22);
+    myOLED.print("C", 23, 53);
+    myOLED.update();
+}
+
+void paintJoystick() {
+    myOLED.drawBitmap(85, 28, joystick32, 32, 29);
+    // myOLED.print("JOY", 90, 22);
+    myOLED.update();
+}
+
+void paintMouse() {
+    myOLED.drawBitmap(85, 30, mouse32, 32, 29);
+    myOLED.print("MOUSE", 86, 22);
+    myOLED.update();
+}
+
+void paintPlay() {
+    myOLED.drawBitmap(51, 28, rew, 24, 24);
+    myOLED.update();
+}
+
+void paintRew() {
+    myOLED.drawBitmap(51, 28, rew, 24, 24);
+    myOLED.update();
+}
+
+void paintStop() {
+    myOLED.drawBitmap(51, 28, stop, 24, 24);
+    myOLED.update();
+}
+
+void paintFF() {
+    myOLED.drawBitmap(51, 28, ff, 24, 24);
+    myOLED.update();
+}
+
+void paintPause() {
+    myOLED.drawBitmap(51, 28, pause, 24, 24);
+    myOLED.update();
+}
+
+void sendKeyEnter() {
+  sendKeyBoardAction(0x5A, true);  // Presionar la tecla ENTER
+  delay(100);                      // Mantener la tecla presionada por 100 ms
+  sendKeyBoardAction(0x5A, false); // Liberar la tecla ENTER
+}
+
+void sendKeyDown() {
+  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, true);  // Presionar la tecla DOWN_ARROW
+  delay(15);
+  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, false); // Liberar la tecla DOWN_ARROW
+}
+
+void sendKeyUp() {
+  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, true);  // Presionar la tecla UP_ARROW
+  delay(15);
+  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, false); // Liberar la tecla UP_ARROW
+}
+
+void sendKeyF1() {
+  sendKeyBoardAction(PS2dev::F1, true);  // Presionar la tecla F1
+  delay(15);
+  sendKeyBoardAction(PS2dev::F1, false); // Liberar la tecla F1
+}
+
+void sendKeyEsc() {
+  sendKeyBoardAction(PS2dev::ESCAPE, true);  // Presionar la tecla ESC
+  delay(15);
+  sendKeyBoardAction(PS2dev::ESCAPE, false); // Liberar la tecla ESC
+}
+
+void selectRapidLoad() {
+    sendKeyF1();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyEnter();
+    sendKeyEnter();
+    sendKeyDown();
+    sendKeyEnter();
+    // //Estando en No para si 
+    sendKeyUp();
+    sendKeyEnter();
+
+    // Estando en SI para NO
+    // sendKeyDown();
+    // sendKeyEnter();  
+    sendKeyEsc();
+    sendKeyEsc();
+    sendKeyEsc();
+    sendKeyEsc();
+}
+
+void selectNormalLoad() {
+    sendKeyF1();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyDown();
+    sendKeyEnter();
+    sendKeyEnter();
+    sendKeyDown();
+    sendKeyEnter();
+    // //Estando en No para si 
+    // sendKeyUp();
+    // sendKeyEnter();
+
+    // Estando en SI para NO
+    sendKeyDown();
+    sendKeyEnter();  
+    sendKeyEsc();
+    sendKeyEsc();
+    sendKeyEsc();
+    sendKeyEsc();
+}
+
+// void select() {
+//     if (selectValue == "CASSETTE") {
+//         selectValue = "SNA";
+//         paintSnap();
+//     } else if (selectValue == "SNA") {
+//         selectValue = "CUMSTON SNA";
+//         paintSnapCumston();
+//     } else if (selectValue == "CUMSTON SNA") {
+//         selectValue = "CASSETTE";
+//         selectNormalLoad();
+//         paintTape();
+//     }
+
+//     Serial.println("--- " + selectValue + " SELECTED");
+// }
+void select() {
+    if (selectValue == "CASSETTE") {
+        selectValue = "SNA";
+        paintSnap();
+    } else if (selectValue == "SNA") {
+        selectValue = "CUMSTON SNA";
+        paintSnapCumston();
+    } else if (selectValue == "CUMSTON SNA") {
+        selectValue = "RAPID";
+        selectNormalLoad();
+        paintTapeRapid();
+    } else if (selectValue == "RAPID") {
+        selectValue = "CASSETTE";
+        
+        paintTapeRapid();
+        paintTape();
+    }
+
+    Serial.println("--- " + selectValue + " SELECTED");
+}
+
+
+void keypadEvent(KeypadEvent key){
+    switch (keypad.getState()){
+        case PRESSED:
+            switch (key) {
+                case '1': // SELECT
+                    select();
+                    break;
+                case '2': // REC
+                    keyCapActive = "REC";
+                    Serial.println("REC PRESSED");
+                    sendKeyBoardAction(PS2dev::F4, true);
+                    break;
+                case '3': // PLAY
+                    keyCapActive = "PLAY";
+                    Serial.println("PLAY PRESSED");
+                    sendKeyBoardAction(PS2dev::F6, true);
+                    break;
+                case '4': // REW
+                    keyCapActive = "REW";
+                    Serial.println("REW PRESSED");
+                    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, true);  // Presionar UP_ARROW
+                    break;
+                case '5': // FF
+                    keyCapActive = "FF";
+                    Serial.println("FF PRESSED");
+                    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, true);  // Presionar UP_ARROW
+                    break;
+                case '6': // STOP/EJECT
+                    // Acción para la tecla '6'
+                    Serial.println("Acción para la tecla 6");
+                    break;
+                case '7': // PAUSE
+                    Serial.println("PAUSE PRESSED");
+                    keyCapActive = "PAUSE";
+                    keyboard.write(0xE1);
+                    delay(15);
+                    keyboard.write(0x14);
+                    delay(15);
+                    keyboard.write(0x77);
+                    delay(15);
+                    keyboard.write(0xE1);
+                    delay(15);
+                    keyboard.write(0xF0);
+                    delay(15);
+                    keyboard.write(0x14);
+                    delay(15);
+                    keyboard.write(0xF0);
+                    delay(15);
+                    keyboard.write(0x77);
+                    delay(15);
+                    break;
+                case '8': // NADA DE NADA
+                    //F1          
+                    break;
+            }
+
+            break;
+        case RELEASED:
+            switch (key) {
+                case '1': // REC
+                    break;
+                case '2': // REC
+                    Serial.println("REC RELEASED");
+                    sendKeyBoardAction(PS2dev::F4, false);
+                    keyCapActive = "NADA";
+                    break;
+                case '3': // PLAY
+                    keyCapActive = "PLAY";
+                    Serial.println("PLAY RELEASED");
+                    sendKeyBoardAction(PS2dev::F6, false);
+                    break;
+                case '4': // REW
+                    keyCapActive = "NADA";
+                    Serial.println("REW RELEASED");
+                    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, false);  // Presionar UP_ARROW
+                    break;
+                case '5': // FF
+                    keyCapActive = "FF";
+                    Serial.println("FF RELEASED");
+                    sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, false);  // Presionar UP_ARROW
+                    break;
+                case '6': // STOP/EJECT
+                    // Acción para la tecla '6'
+                    Serial.println("Acción para la tecla 6");
+                    break;
+                case '7': // PAUSE
+                    Serial.println("PAUSE RELEASED");
+                    break;
+                case '8': // NADA DE NADA
+                    break;
+            }
+            break;
+        case HOLD:
+            // Acción para mantener presionada una tecla    
+            break;
+        case IDLE:
+            // Acción para cuando no se presiona ninguna tecla
+            break;    }
+}
 
 void setup() {
     Serial.begin(9600);
+    selectValue = "CASSETTE";
     myOLED.begin(SSD1306_128X64);
     myOLED.setBrightness(255);
-    myOLED.rotateDisplay(true);
     myOLED.setFont(SmallFont);
-    displayTextOLED("SNAPSHOT");
+    myOLED.clrScr();
+    myOLED.drawRoundRect(0,63,127,16);
+    paintTape();
+    paintJoystick();
+    // myOLED.drawBitmap(10, 30, snapshot32, 32, 29);
+    // myOLED.drawBitmap(94, 30, mouse32, 24, 24);
+    // myOLED.drawBitmap(51, 28, clearImage, 24, 24);
+    // myOLED.print("SNAP", 11, 22);
+    // myOLED.print("MOUSE", 86, 22);
+    // myOLED.print("C", 23, 52);
+    // myOLED.drawRoundRect(0,63,127,16);
+    // myOLED.drawRoundRect(40,63,64,6);
 
+    // myOLED.update();
+    keypad.addEventListener(keypadEvent);
 }
 
 void loop()
 {
-
-    char cassetteSwich = cassette.getKey();
+    char key = keypad.getKey();
+    // char cassetteSwich = cassette.getKey();
     currentState = controller.getState();
     gamePadState();
 
-    switch (cassetteSwich) {
-        case '1': // SELECT
-            select();
-            break;
-        case '2': // REC
-            Rec();
-            break;
-        case '3': // PLAY
-            Play();
-            break;
-        case '4': // REW
-            Rew();
-            break;
-        case '5': // FF
-            FF();
-            break;
-        case '6': // STOP/EJECT
-            // Acción para la tecla '6'
-            Serial.println("Acción para la tecla 6");
-            break;
-        case '7': // PAUSE
-            Pause();
-            break;
-        case '8': // NADA DE NADA
-            break;
-    }
+
 }
