0d91fb91480fd2ae246ac5da040c500783f015da
destroyer <destroyer.dcf@gmail.com>
2024-12-30 13:42:29 +0100

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -51,234 +51,145 @@ void displayTextOLED(const String& text) {
     myOLED.update();
 }
 
-void sendGamePadAction(int scancode, bool press) {
-  keyboard.write(0xE2); // Prefijo para gamepad
-  delay(15);
-  if (!press) {
-    keyboard.write(0xF0); // Código de liberación
-    delay(15);
-  }
-  keyboard.write(scancode);
-  delay(15);
-}
-
-void sendKeyBoardAction(int scancode, bool press) {
-  if (press) {
-    keyboard.write(scancode);
-  } else {
-    keyboard.write(0xF0); // Código de liberación
-    delay(15);
-    keyboard.write(scancode);
-  }
-  delay(15);
-}
-
-void sendKeyBoardAction2(int scancode, bool press) {
-  if (press) {
-    keyboard.write(scancode);
-  } else {
-    keyboard.write(0xF0); // Código de liberación
-    delay(15);
-    keyboard.write(scancode);
-  }
-  delay(180);
-}
-
-
-
-void sendSpecialKeyBoardAction(int scancode, bool press) {
-  keyboard.write(0xE0); // Prefijo para teclas especiales
-  delay(15);
-  if (!press) {
-    keyboard.write(0xF0); // Código de liberación
-    delay(15);
-  }
-  keyboard.write(scancode);
-  delay(15);
-}
-void sendSpecialKeyBoardAction2(int scancode, bool press) {
-  keyboard.write(0xE0); // Prefijo para teclas especiales
-  delay(15);
-  if (!press) {
-    keyboard.write(0xF0); // Código de liberación
-    delay(15);
-  }
-  keyboard.write(scancode);
-  delay(100);
-}
-void sendControlEnter() {
-  // Presionar la tecla CONTROL
-  sendSpecialKeyBoardAction2(PS2dev::SpecialScanCodes::RIGHT_CONTROL, true);
-  sendSpecialKeyBoardAction2(PS2dev::SpecialScanCodes::NUMPAD_ENTER, true);
-  sendSpecialKeyBoardAction2(PS2dev::SpecialScanCodes::NUMPAD_ENTER, false);
-  sendSpecialKeyBoardAction2(PS2dev::SpecialScanCodes::RIGHT_CONTROL, false);
-}
-
-// void StopEject() {
-//     if (selectValue == "CASSETTE") {
-//         sendKeyBoardAction(PS2dev::F5, true);
-//     } else if (selectValue == "SNA") {
-//         sendKeyBoardAction(PS2dev::F2, true);
-//     } else if (selectValue == "CUSMTON SNA") {
-//         sendKeyBoardAction(PS2dev::F3, true);
-//     } else if (selectValue == "RAPIDA") {
-//         sendKeyBoardAction(PS2dev::F4, true);
-//     }
-//     // sendKeyBoardAction(PS2dev::F5, true);
-//     // sendKeyBoardAction(PS2dev::LEFT_SHIFT, true);
-//     // delay(100);
-//     // sendKeyBoardAction(PS2dev::F6, true);    
-// }
-
-
-void Insert() {
-    keyCapActive = "INSERT";
-    sendKeyBoardAction(PS2dev::LEFT_CONTROL, true);  // Presionar la tecla DOWN_ARROW
-    delay(100);
-    sendKeyBoardAction(PS2dev::ENTER, true);    
-}
-
-
-
 void gamePadState()
 {
     // Verificar el cambio de estado para cada botón
     if ((currentState & SC_BTN_UP) != (lastState & SC_BTN_UP)) {
         if (currentState & SC_BTN_UP) {
             Serial.println("Botón UP presionado");
-            sendGamePadAction(PS2dev::ESP_JOY1UP, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1UP);
         } else {
             Serial.println("Botón UP soltado");
-            sendGamePadAction(PS2dev::ESP_JOY1UP, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1UP);
         }
     }
 
     if ((currentState & SC_BTN_DOWN) != (lastState & SC_BTN_DOWN)) {
         if (currentState & SC_BTN_DOWN) {
-            sendGamePadAction(PS2dev::ESP_JOY1DOWN, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1DOWN);
             Serial.println("Botón DOWN presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1DOWN, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1DOWN);
             Serial.println("Botón DOWN soltado");
         }
     }
 
     if ((currentState & SC_BTN_LEFT) != (lastState & SC_BTN_LEFT)) {
         if (currentState & SC_BTN_LEFT) {
-            sendGamePadAction(PS2dev::ESP_JOY1LEFT, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1LEFT);
             Serial.println("Botón LEFT presionado");
         } else {
-          sendGamePadAction(PS2dev::ESP_JOY1LEFT, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1LEFT);
             Serial.println("Botón LEFT soltado");
         }
     }
 
     if ((currentState & SC_BTN_RIGHT) != (lastState & SC_BTN_RIGHT)) {
         if (currentState & SC_BTN_RIGHT) {
-            sendGamePadAction(PS2dev::ESP_JOY1RIGHT, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1RIGHT);
             Serial.println("Botón RIGHT presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1RIGHT, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1RIGHT);
             Serial.println("Botón RIGHT soltado");
         }
     }
 
     if ((currentState & SC_BTN_START) != (lastState & SC_BTN_START)) {
         if (currentState & SC_BTN_START) {
-            sendGamePadAction(PS2dev::ESP_JOY1START, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1START);
             Serial.println("Botón START presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1START, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1START);
             Serial.println("Botón START soltado");
         }
     }
 
     if ((currentState & SC_BTN_A) != (lastState & SC_BTN_A)) {
         if (currentState & SC_BTN_A) {
-            sendGamePadAction(PS2dev::ESP_JOY1A, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1A);
             Serial.println("Botón A presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1A, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1A);
             Serial.println("Botón A soltado");
         }
     }
 
     if ((currentState & SC_BTN_B) != (lastState & SC_BTN_B)) {
         if (currentState & SC_BTN_B) {
-            sendGamePadAction(PS2dev::ESP_JOY1B, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1B);
             Serial.println("Botón B presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1B, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1B);
             Serial.println("Botón B soltado");
         }
     }
 
     if ((currentState & SC_BTN_C) != (lastState & SC_BTN_C)) {
         if (currentState & SC_BTN_C) {
-            sendGamePadAction(PS2dev::ESP_JOY1C, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1C);
             Serial.println("Botón C presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1C, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1C);
             Serial.println("Botón C soltado");
         }
     }
 
     if ((currentState & SC_BTN_X) != (lastState & SC_BTN_X)) {
         if (currentState & SC_BTN_X) {
-            sendGamePadAction(PS2dev::ESP_JOY1X, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1X);
             Serial.println("Botón X presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1X, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1X);
             Serial.println("Botón X soltado");
         }
     }
 
     if ((currentState & SC_BTN_Y) != (lastState & SC_BTN_Y)) {
         if (currentState & SC_BTN_Y) {
-            sendGamePadAction(PS2dev::ESP_JOY1Y, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Y);
             Serial.println("Botón Y presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1Y, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Y);
             Serial.println("Botón Y soltado");
         }
     }
 
     if ((currentState & SC_BTN_Z) != (lastState & SC_BTN_Z)) {
         if (currentState & SC_BTN_Z) {
-            sendGamePadAction(PS2dev::ESP_JOY1Z, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Z);
             Serial.println("Botón Z presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1Z, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Z);
             Serial.println("Botón Z soltado");
         }
     }
 
     if ((currentState & SC_BTN_1) != (lastState & SC_BTN_1)) {
         if (currentState & SC_BTN_1) {
-            sendGamePadAction(PS2dev::ESP_JOY1A, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1A);
             Serial.println("Botón 1 presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1A, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1A);
             Serial.println("Botón 1 soltado");
         }
     }
 
     if ((currentState & SC_BTN_2) != (lastState & SC_BTN_2)) {
         if (currentState & SC_BTN_2) {
-            sendGamePadAction(PS2dev::ESP_JOY1Z, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Z);
             Serial.println("Botón 2 presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1Z, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1Z);
             Serial.println("Botón 2 soltado");
         }
     }
 
     if ((currentState & SC_BTN_MODE) != (lastState & SC_BTN_MODE)) {
         if (currentState & SC_BTN_MODE) {
-            sendGamePadAction(PS2dev::ESP_JOY1MODE, true);
+            keyboard.keyboard_press_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1MODE);
             Serial.println("Botón MODE presionado");
         } else {
-            sendGamePadAction(PS2dev::ESP_JOY1MODE, false);
+            keyboard.keyboard_release_ESPectrum_special(PS2dev::ESPectrumSpecialScanCodes::ESP_JOY1MODE);
             Serial.println("Botón MODE soltado");
         }
     }
@@ -289,21 +200,8 @@ void gamePadState()
 
 void paintTape() {
     myOLED.drawBitmap(10, 26, cassette32, 32, 32);
-    // myOLED.print("      ", 11, 22);
-    // myOLED.print("N", 24, 22);
-    // myOLED.drawLine(10,26,22,26);
-    // myOLED.drawLine(32,26,42,26);
     myOLED.update();
 }
-
-// void paintTapeRapid() {
-//     myOLED.drawBitmap(10, 28, cassette32, 32, 32);
-//     myOLED.print("      ", 11, 22);
-//     myOLED.print("R", 24, 22);
-//     myOLED.drawLine(10,26,22,26);
-//     myOLED.drawLine(32,26,42,26);
-//     myOLED.update();
-// }
 
 void paintSnap() {
     myOLED.drawBitmap(10, 26, snapshot32, 32, 29);
@@ -354,137 +252,6 @@ void paintPause() {
     myOLED.drawBitmap(51, 28, pause, 24, 24);
     myOLED.update();
 }
-
-void sendKeyEnter() {
-  sendKeyBoardAction(0x5A, true);  // Presionar la tecla ENTER
-  delay(100);                      // Mantener la tecla presionada por 100 ms
-  sendKeyBoardAction(0x5A, false); // Liberar la tecla ENTER
-}
-
-void sendKeyDown() {
-  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, true);  // Presionar la tecla DOWN_ARROW
-  delay(15);
-  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::DOWN_ARROW, false); // Liberar la tecla DOWN_ARROW
-}
-
-void sendKeyUp() {
-  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, true);  // Presionar la tecla UP_ARROW
-  delay(15);
-  sendSpecialKeyBoardAction(PS2dev::SpecialScanCodes::UP_ARROW, false); // Liberar la tecla UP_ARROW
-}
-
-void sendKeyF1() {
-  sendKeyBoardAction(PS2dev::F1, true);  // Presionar la tecla F1
-  delay(15);
-  sendKeyBoardAction(PS2dev::F1, false); // Liberar la tecla F1
-}
-
-void sendKeyEsc() {
-  sendKeyBoardAction(PS2dev::ESCAPE, true);  // Presionar la tecla ESC
-  delay(15);
-  sendKeyBoardAction(PS2dev::ESCAPE, false); // Liberar la tecla ESC
-}
-
-// void selectRapidLoad() {
-//     sendKeyF1();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyEnter();
-//     sendKeyEnter();
-//     sendKeyDown();
-//     sendKeyEnter();
-//     // //Estando en No para si 
-//     sendKeyUp();
-//     sendKeyEnter();
-
-//     // Estando en SI para NO
-//     // sendKeyDown();
-//     // sendKeyEnter();  
-//     sendKeyEsc();
-//     sendKeyEsc();
-//     sendKeyEsc();
-//     sendKeyEsc();
-// }
-
-// void selectNormalLoad() {
-//     sendKeyF1();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyDown();
-//     sendKeyEnter();
-//     sendKeyEnter();
-//     sendKeyDown();
-//     sendKeyEnter();
-//     // //Estando en No para si 
-//     // sendKeyUp();
-//     // sendKeyEnter();
-
-//     // Estando en SI para NO
-//     sendKeyDown();
-//     sendKeyEnter();  
-//     sendKeyEsc();
-//     sendKeyEsc();
-//     sendKeyEsc();
-//     sendKeyEsc();
-// }
-
-// void select() {
-//     if (selectValue == "CASSETTE") {
-//         selectValue = "SNA";
-//         paintSnap();
-//     } else if (selectValue == "SNA") {
-//         selectValue = "CUSMTON SNA";
-//         paintSnapCusmton();
-//     } else if (selectValue == "CUSMTON SNA") {
-//         selectValue = "CASSETTE";
-//         selectNormalLoad();
-//         paintTape();
-//     }
-
-//     Serial.println("--- " + selectValue + " SELECTED");
-// }
-// void select() {
-//     if (selectValue == "CASSETTE") {
-//         selectValue = "SNA";
-//         paintSnap();
-//     } else if (selectValue == "SNA") {
-//         selectValue = "CUSMTON SNA";
-//         paintSnapCusmton();
-//     } else if (selectValue == "CUSMTON SNA") {
-//         selectValue = "RAPID";
-//         selectNormalLoad();
-//         paintTapeRapid();
-//     } else if (selectValue == "RAPID") {
-//         selectValue = "CASSETTE";
-        
-//         paintTapeRapid();
-//         paintTape();
-//     }
-
-//     Serial.println("--- " + selectValue + " SELECTED");
-// }
-// void select() {
-//     selectValue = (selectValue + 1) % 3; // Incrementar y ciclar entre 0 y 2
-
-//     switch (selectValue) {
-//         case 0:
-//             paintTape();
-//             Serial.println("--- CASSETTE SELECTED, Value: " + String(selectValue));
-//             keyboard.keyboard_press(PS2dev::ESCAPE);
-//             break;
-//         case 1:
-//             paintSnap();
-//             Serial.println("--- SNA SELECTED, Value: " + String(selectValue));
-//             break;
-//         case 2:
-//             paintSnapCusmton();
-//             Serial.println("--- CUSMTON SNA SELECTED, Value: " + String(selectValue));
-//             break;
-//     }
-// }
 
 void keypadEvent(KeypadEvent key){
     switch (keypad.getState()){
@@ -545,35 +312,10 @@ void keypadEvent(KeypadEvent key){
                     Serial.println("PAUSE PRESSED");
                     keyboard.key_pause();
                     keyCapActive = "PAUSE";
-                    // keyboard.write(0xE1);
-                    // delay(15);
-                    // keyboard.write(0x14);
-                    // delay(15);
-                    // keyboard.write(0x77);
-                    // delay(15);
-                    // keyboard.write(0xE1);
-                    // delay(15);
-                    // keyboard.write(0xF0);
-                    // delay(15);
-                    // keyboard.write(0x14);
-                    // delay(15);
-                    // keyboard.write(0xe0);
-                    // delay(15);
-                    // keyboard.write(0x77);
-                    // delay(15);
                     break;
                 case '8': // NADA DE NADA
                     // keyboard.key_break();
                     keyboard.keyboard_press(PS2dev::ESCAPE);
-                                    // sendControlEnter();
-                    ///////sendKeyBoardAction(PS2dev::LEFT_CONTROL, true);
-                    //sendSpecialKeyBoardAction2(PS2dev::SpecialScanCodes::RIGHT_CONTROL, true);
-                    // sendKeyBoardAction(PS2dev::LEFT_CONTROL, true);
-                    // delay(1000); // Aumentar el tiempo de espera para asegurar la pulsación
-                    // sendKeyBoardAction(PS2dev::LEFT_CONTROL, false);
-                    ///////sendKeyBoardAction(PS2dev::ENTER, true);
-                    // delay(100); // Aumentar el tiempo de espera para asegurar la pulsación
-                    
                     break;
             }
 
@@ -640,23 +382,12 @@ void setup() {
     myOLED.drawRoundRect(0,63,127,16);
     paintTape();
     paintJoystick();
-    // myOLED.drawBitmap(10, 30, snapshot32, 32, 29);
-    // myOLED.drawBitmap(94, 30, mouse32, 24, 24);
-    // myOLED.drawBitmap(51, 28, clearImage, 24, 24);
-    // myOLED.print("SNAP", 11, 22);
-    // myOLED.print("MOUSE", 86, 22);
-    // myOLED.print("C", 23, 52);
-    // myOLED.drawRoundRect(0,63,127,16);
-    // myOLED.drawRoundRect(40,63,64,6);
-
-    // myOLED.update();
     keypad.addEventListener(keypadEvent);
 }
 
 void loop()
 {
     keypad.getKey();
-    // char cassetteSwich = cassette.getKey();
 
     currentState = controller.getState();
     gamePadState();
